35b2f4206ec0d8010c56c30b1296941459c04785
akramhelil <akramhelil@gmail.com>
2019-05-04 20:52:14 -0400

    user library straightened added margin to the left 20

--- mod4-front/src/UserLibrary.js
+++ mod4-front/src/UserLibrary.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import VideoCard from './VideoCard';
 
-
+const userlibStyle = {
+    marginLeft: 20
+ }
 export default class UserLibrary extends Component {
     render() {
         return (
-            <div>
+            <div style={userlibStyle}>
                 <VideoCard video={this.props.video} />
                 <br/>
 
