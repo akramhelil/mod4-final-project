923ea86b4dee92219cae032dc4c136635160f5d1
github username <github email address>
2019-05-06 14:09:23 -0400

    auth_controller added

--- mod4-back/app/models/user.rb
+++ mod4-back/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :favorites
     has_many :videos, through: :favorites
+
+    has_secure_password
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
